f12f766d557cc9ba1942064bb5e0afdd40e359af
pp <3264705273@qq.com>
2026-03-03 15:10:04 +0800

    初始化

---
+++ src/main/java/org/example/tsevaluationsystem/TSEvaluationSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.tsevaluationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TSEvaluationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TSEvaluationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tsevaluationsystem/TSEvaluationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tsevaluationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TSEvaluationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
